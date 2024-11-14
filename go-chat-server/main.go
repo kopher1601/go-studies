665744499001edd99e145b765c333f4cf2932958
kopher <kopher1601@gmail.com>
2024-11-14 22:39:01 +0900

    go-chat-server feat: flag

--- go-chat-server/main.go
+++ go-chat-server/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "go-chat-server/network"
+import (
+	"flag"
+	"fmt"
+	"go-chat-server/config"
+	"go-chat-server/network"
+)
+
+var pathFlag = flag.String("config", "./config.toml", "config set")
+var port = flag.String("port", ":1010", "port set")
 
 func main() {
+	flag.Parse()
+	c := config.NewConfig(*pathFlag)
+	fmt.Println(c)
+
 	n := network.NewServer()
 
 	err := n.StartServer()
