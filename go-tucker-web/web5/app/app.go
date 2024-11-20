bd9be729c0eca993249b98643983375cc8c958db
kopher <kopher1601@gmail.com>
2024-11-21 08:23:56 +0900

    go-tucker-web: restful api 6

--- go-tucker-web/web5/app/app.go
+++ go-tucker-web/web5/app/app.go
@@ -1,10 +1,24 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
+	"time"
 )
+
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+var userMap map[int]*User
+var lastId int
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World")
@@ -16,14 +30,54 @@ func usersHandler(w http.ResponseWriter, r *http.Request) {
 
 func getUserInfoHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	fmt.Fprint(w, "User Id: ", vars["id"])
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	u, ok := userMap[id]
+	if !ok {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "No User ID :", id)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data, _ := json.Marshal(u)
+	fmt.Fprint(w, string(data))
+}
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	u := &User{}
+	err := json.NewDecoder(r.Body).Decode(u)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	// Created User
+	lastId++
+	u.ID = lastId
+	u.CreatedAt = time.Now()
+	userMap[u.ID] = u
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	jsonBytes, _ := json.Marshal(u)
+	fmt.Fprint(w, string(jsonBytes))
 }
 
 func NewHandler() http.Handler {
+	userMap = make(map[int]*User)
+	lastId = 0
 	mux := mux.NewRouter()
 
 	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/users", usersHandler)
+	mux.HandleFunc("/users", usersHandler).Methods("GET")
+	mux.HandleFunc("/users", createUserHandler).Methods("POST")
 	mux.HandleFunc("/users/{id:[0-9]+}", getUserInfoHandler)
 	return mux
 }
--- go-tucker-web/web5/app/app_test.go
+++ go-tucker-web/web5/app/app_test.go
@@ -1,10 +1,13 @@
 package app
 
 import (
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -44,5 +47,46 @@ func TestGetUserInfo(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal(http.StatusOK, resp.StatusCode)
 	data, _ := io.ReadAll(resp.Body)
-	assert.Contains(string(data), "User Id: 89")
+	assert.Contains(string(data), "User ID :89")
+}
+
+func TestCreateUser(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+
+	dto := struct {
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+		Email     string `json:"email"`
+	}{
+		FirstName: "Kakao",
+		LastName:  "Daum",
+		Email:     "line@line.com",
+	}
+	jsonBytes, _ := json.Marshal(dto)
+	resp, err := http.Post(
+		ts.URL+"/users",
+		"application/json",
+		strings.NewReader(string(jsonBytes)),
+	)
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, resp.StatusCode)
+
+	u := &User{}
+	err = json.NewDecoder(resp.Body).Decode(u)
+	assert.NoError(err)
+	assert.NotEqual(0, u.ID)
+
+	id := u.ID
+	resp, err = http.Get(ts.URL + "/users/" + strconv.Itoa(id))
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+
+	u2 := &User{}
+	err = json.NewDecoder(resp.Body).Decode(u2)
+	assert.NoError(err)
+	assert.Equal(id, u2.ID)
+	assert.Equal(u.FirstName, u2.FirstName)
 }
