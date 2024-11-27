cc318a9b31c899051b37cf1e11c93876810049fd
kopher <kopher1601@gmail.com>
2024-11-28 08:55:05 +0900

    http-server: Refactor - 이슈수정

---
+++ go-tdd/http-server/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/player/")
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
+}
+
+// 애플리케이션 실행
