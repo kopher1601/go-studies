8599cc0963fae82ee9a54c0bec3446f5f4488f0a
kopher <kopher1601@gmail.com>
2025-01-04 07:35:31 +0900

    go-gin : JWT

--- go-gin/services/auth_service.go
+++ go-gin/services/auth_service.go
@@ -1,14 +1,19 @@
 package services
 
 import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
 	"go-gin/models"
 	"go-gin/repositories"
 	"golang.org/x/crypto/bcrypt"
+	"os"
+	"time"
 )
 
 type AuthService interface {
 	Signup(email, password string) error
 	Login(email, password string) (*string, error)
+	GetUserFromToken(tokenString string) (*models.User, error)
 }
 
 type authService struct {
@@ -42,5 +47,49 @@ func (a *authService) Login(email, password string) (*string, error) {
 		return nil, err
 	}
 
-	return &foundUser.Email, nil
+	token, err := CreateToken(foundUser.ID, foundUser.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func CreateToken(useId uint, email string) (*string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub":   useId,
+		"email": email,
+		"exp":   time.Now().Add(time.Hour).Unix(),
+	})
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+	if err != nil {
+		return nil, err
+	}
+	return &tokenString, nil
+}
+
+func (a *authService) GetUserFromToken(tokenString string) (*models.User, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// 서명 방법의 확인
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var user *models.User
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			return nil, jwt.ErrTokenExpired
+		}
+
+		user, err = a.repository.FindUser(claims["email"].(string))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return user, nil
 }
