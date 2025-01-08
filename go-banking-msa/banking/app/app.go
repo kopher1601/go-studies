81dc7293b9112fc6cddf830cc5639bfdb8264474
kopher <kopher1601@gmail.com>
2025-01-09 01:22:09 +0900

    go-banking : gorilla/mux

--- go-banking-msa/banking/app/app.go
+++ go-banking-msa/banking/app/app.go
@@ -1,15 +1,31 @@
 package app
 
 import (
+	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func Start() {
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/customers", getAllCustomers)
+	//router := http.NewServeMux()
+	router := mux.NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8000", mux))
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Post request received")
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprint(w, vars["customer_id"])
 }
