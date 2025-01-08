ed3bd3db1df4955c930539facaee672c31ae3839
kopher <kopher1601@gmail.com>
2025-01-09 00:06:06 +0900

    go-banking : Json Encoding

--- go-banking-msa/banking/main.go
+++ go-banking-msa/banking/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zipcode"`
+}
+
 func main() {
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{Name: "Alice", City: "London", Zipcode: "1234"},
+		{Name: "Job", City: "Manchester", Zipcode: "1234"},
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
 }
