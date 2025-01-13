a4a32f8fd677153f807c977ffe77a9108c69032d
kopher <kopher1601@gmail.com>
2025-01-13 22:40:19 +0900

    go-banking : CustomerRepositoryStub

--- go-banking-msa/banking/domain/customer.go
+++ go-banking-msa/banking/domain/customer.go
@@ -8,3 +8,28 @@ type Customer struct {
 	DateOfBirth string
 	Status      string
 }
+
+type CustomerRepository interface {
+	FindAll() ([]Customer, error)
+}
+
+type CustomerRepositoryStub struct {
+	customers []Customer
+}
+
+func (s *CustomerRepositoryStub) FindAll() ([]Customer, error) {
+	return s.customers, nil
+}
+
+func NewCustomerRepositoryStub() CustomerRepositoryStub {
+	customers := []Customer{
+		{
+			ID:          "1001",
+			Name:        "Ashish",
+			City:        "Tokyo",
+			Zipcode:     "111-1111",
+			DateOfBirth: "1999-09-09",
+			Status:      "1",
+		},
+	}
+}
