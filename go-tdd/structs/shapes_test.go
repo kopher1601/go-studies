50a2c08055607d34dd237a1680d675db8edc57e0
kopher <kopher1601@gmail.com>
2025-01-30 21:40:59 +0900

    go-tdd : structs - finish

--- go-tdd/structs/shapes_test.go
+++ go-tdd/structs/shapes_test.go
@@ -16,14 +16,20 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{
-		Width:  12.0,
-		Height: 6.0,
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
-	got := Area(rectangle)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
 	}
 }
