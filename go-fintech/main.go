a42cdafe457f74c51899d8507a668205b30e7ad7
kopher <kopher1601@gmail.com>
2025-02-20 00:15:45 +0900

    go-fintech : Set up log

--- go-fintech/main.go
+++ go-fintech/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"go-fintech/config"
+	"go-fintech/utils"
+	"log"
 )
 
 func main() {
+	utils.LoggingSettings(config.Config.LogFile)
+	log.Println("test")
 	fmt.Println(config.Config.ApiKey)
 	fmt.Println(config.Config.ApiSecret)
 }
