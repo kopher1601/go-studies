b4c8f17f3ba02e65909dd9a2d3023323aaee6954
kopher <kopher1601@gmail.com>
2025-02-23 11:43:51 +0900

    go-fintech : Create DB Schemas

--- go-fintech/go.mod
+++ go-fintech/go.mod
@@ -4,6 +4,7 @@ go 1.23
 
 require (
 	github.com/gorilla/websocket v1.5.3
+	github.com/mattn/go-sqlite3 v1.14.24
 	gopkg.in/ini.v1 v1.67.0
 )
 
--- go-fintech/main.go
+++ go-fintech/main.go
@@ -2,24 +2,12 @@ package main
 
 import (
 	"fmt"
-	"go-fintech/bitflyer"
+	"go-fintech/app/models"
 	"go-fintech/config"
 	"go-fintech/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-
-	order := &bitflyer.Order{
-		ProductCode:     config.Config.ProductCode,
-		ChildOrderType:  "MARKET",
-		Side:            "BUY",
-		Size:            0.01,
-		MinuteToExpires: 1,
-		TimeInForce:     "GTC",
-	}
-	res, _ := apiClient.SendOrder(order)
-	fmt.Println(res.ChildOrderAcceptanceID)
-
+	fmt.Println(models.DbConnection)
 }
