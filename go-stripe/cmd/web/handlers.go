8137d895e1b92f18ed29a2f2e6aba2c18ca3279b
kopher <kopher1601@gmail.com>
2025-05-26 01:30:36 +0900

    remove go-stripe

--- go-stripe/cmd/web/handlers.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "net/http"
-
-func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
-	app.infoLog.Println("Hit the handler")
-}
--- go-stripe/cmd/web/render.go
+++
@@ -1,7 +0,0 @@
-package main
-
-type templateData struct {
-	StringMap map[string]string
-	IntMap    map[string]int
-	FloatMap  map[string]float32
-}
