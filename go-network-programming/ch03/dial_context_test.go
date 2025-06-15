d00a55387be0e854af7e6672eba7bc0cd99e406f
kopher <kopher1601@gmail.com>
2025-06-15 10:45:31 +0900

    Add test for DialContext functionality with deadline handling in chapter 3

---
+++ go-network-programming/ch03/dial_context_test.go
@@ -0,0 +1,38 @@
+package ch03
+
+import (
+	"context"
+	"net"
+	"syscall"
+	"testing"
+	"time"
+)
+
+func TestDialContext(t *testing.T) {
+	dl := time.Now().Add(5 * time.Second)
+	ctx, cancel := context.WithDeadline(context.Background(), dl)
+	defer cancel()
+
+	var d net.Dialer // DialContext는 Dialer의 메서드입니다.
+	d.Control = func(_, _ string, _ syscall.RawConn) error {
+		// 컨텍스트의 데드라인이 지나기 위해 충분히 긴 시간동안 대기합니다.
+		time.Sleep(5*time.Second + time.Millisecond)
+		return nil
+	}
+	conn, err := d.DialContext(ctx, "tcp", "10.0.0.0:80")
+	if err == nil {
+		conn.Close()
+		t.Fatal("connection did not time out")
+	}
+	nErr, ok := err.(net.Error)
+	if !ok {
+		t.Error(err)
+	} else {
+		if !nErr.Timeout() {
+			t.Errorf("error is not a timeout: %v", err)
+		}
+	}
+	if ctx.Err() != context.DeadlineExceeded {
+		t.Errorf("expected deadline exceeded; got %v", ctx.Err())
+	}
+}
