dd0a35e9736d3dc2804871af8618fd8a88e9f1a5
kopher <kopher1601@gmail.com>
2025-07-01 08:24:29 +0900

    Add unit tests for a simple HTTP server in chapter 9

---
+++ go-network/ch09/server_test.go
@@ -0,0 +1,51 @@
+package ch09
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestSimpleHTTPServer(t *testing.T) {
+	srv := &http.Server{
+		Addr: "127.0.0.1:8081",
+		Handler: http.TimeoutHandler(
+			handlers.DefaultHandler(),
+			2*time.Minute,
+			"",
+		),
+		IdleTimeout: 5 * time.Minute,
+		ReadTimeout: time.Minute,
+	}
+
+	l, err := net.Listen("tcp", srv.Addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	go func() {
+		err := srv.Serve(l)
+		if err != http.ErrServerClosed {
+			t.Fatal(err)
+		}
+	}()
+
+	testCases := []struct {
+		method   string
+		body     io.Reader
+		code     int
+		response string
+	}{
+		{http.MethodGet, nil, http.StatusOK, "Hello, friend!"},
+		{http.MethodPost, bytes.NewBufferString("<world>"), http.StatusOK, "Hello, &lt;world&gt;!"},
+		{http.MethodHead, nil, http.StatusMethodNotAllowed, ""},
+	}
+
+	client := new(http.Client)
+	path := fmt.Sprintf("http://%s/", srv.Addr)
+
+}
