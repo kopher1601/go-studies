95a1e755d5887dffd71f1dd5b7e2d9479c38f099
kopher <kopher1601@gmail.com>
2025-09-19 00:10:08 +0900

    refactor: Update Hello function to accept a name argument and adjust tests accordingly

--- go-tdd/README.md
+++ go-tdd/README.md
@@ -9,3 +9,9 @@ go test ./hello-word/
 # または全体テスト
 go test ./...
 ```
+
+## Formatting
+```go
+//  %q は値を二重引用符で囲む
+t.Errorf("got %q want %q", got, want)
+```
--- go-tdd/hello-world/hello_test.go
+++ go-tdd/hello-world/hello_test.go
@@ -5,10 +5,11 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Chris")
+	want := "Hello, Chris"
 
 	if got != want {
+		//  %q は値を二重引用符で囲む
 		t.Errorf("got %q want %q", got, want)
 	}
 }
