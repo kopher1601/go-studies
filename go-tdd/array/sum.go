f90277c875354878dda4e3e126c8d5dd3f9c342c
kopher <kopher1601@gmail.com>
2025-09-23 12:59:44 +0900

    Arrays

--- go-tdd/array/sum.go
+++ go-tdd/array/sum.go
@@ -7,3 +7,25 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbers ...[]int) []int {
+	var sums []int
+	for _, number := range numbers {
+		sums = append(sums, Sum(number))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbers ...[]int) []int {
+	var sums []int
+	for _, number := range numbers {
+		if len(number) > 0 {
+			tail := number[1:]
+			sums = append(sums, Sum(tail))
+		} else {
+			sums = append(sums, 0)
+		}
+	}
+	return sums
+}
